ecd7b02a3af8f0209c38f13faebe8ba4ffbf2c30
Jaap Jansma <jaap.jansma@civicoop.org>
2019-04-18 16:33:00 +0200

    Changed generator so it will use the Study Number field of the study and not the id field

--- CRM/Nihrnumbergenerator/StudyParticipantNumberGenerator.php
+++ CRM/Nihrnumbergenerator/StudyParticipantNumberGenerator.php
@@ -6,16 +6,17 @@
 
 class CRM_Nihrnumbergenerator_StudyParticipantNumberGenerator {
 
-  public static function generateNumber($study_id, $contact_id, $case_id) {
+  public static function generateNumber($study_number, $contact_id, $case_id) {
     // First check for an existing number, if it exists return it
     $sql = "SELECT participation_data.nvpd_study_participant_id AS id 
             FROM civicrm_value_nihr_participation_data participation_data 
             INNER JOIN civicrm_case_contact case_contact ON case_contact.case_id = participation_data.entity_id
             INNER JOIN civicrm_value_nihr_project_data project_data ON project_data.entity_id = participation_data.nvpd_project_id
-            WHERE project_data.npd_study_id = %1 and case_contact.contact_id = %2 AND  participation_data.nvpd_study_participant_id IS NOT NULL
+            INNER JOIN civicrm_nihr_study study ON study.id = project_data.npd_study_id
+            WHERE study.study_number = %1 and case_contact.contact_id = %2 AND  participation_data.nvpd_study_participant_id IS NOT NULL
             ORDER BY id DESC
             LIMIT 0, 1";
-    $sqlParams[1] = array($study_id, 'Integer');
+    $sqlParams[1] = array($study_number, 'String');
     $sqlParams[2] = array($contact_id, 'Integer');
     $existingId = CRM_Core_DAO::singleValueQuery($sql, $sqlParams);
     if ($existingId) {
@@ -27,11 +28,14 @@ class CRM_Nihrnumbergenerator_StudyParticipantNumberGenerator {
             SELECT COUNT(DISTINCT participation_data.`nvpd_study_participant_id`) 
             FROM `civicrm_value_nihr_participation_data` participation_data
             INNER JOIN civicrm_value_nihr_project_data project_data ON project_data.entity_id = participation_data.nvpd_project_id 
-            WHERE project_data.npd_study_id = %1 AND participation_data.`nvpd_study_participant_id` IS NOT NULL ";
-    $sequenceNrSqlParams[1] = array($study_id, 'Integer');
+            INNER JOIN civicrm_nihr_study study ON study.id = project_data.npd_study_id
+            WHERE study.study_number = %1 AND participation_data.`nvpd_study_participant_id` IS NOT NULL ";
+    $sequenceNrSqlParams[1] = array($study_number, 'String');
     $newSequenceNr = CRM_Core_DAO::singleValueQuery($sequenceNrSql, $sequenceNrSqlParams);
     $sql = "SELECT COUNT(*) FROM civicrm_value_nihr_participation_data participation_data WHERE nvpd_study_participant_id = %1";
-    $studyCode = 'SP'.str_pad($study_id, 4, 0, STR_PAD_LEFT);
+
+    $studyNumberWithouthPrefix = preg_replace("#(NBR|CBR-?)([0-9]+)#", "$2", $study_number);
+    $studyCode = 'SP'.str_pad($studyNumberWithouthPrefix, 4, 0, STR_PAD_LEFT);
 
     do {
       $newSequenceNr++;
@@ -55,15 +59,17 @@ class CRM_Nihrnumbergenerator_StudyParticipantNumberGenerator {
   public static function createNewNumberForCase($case_id) {
     $sql ="SELECT 
             civicrm_case_contact.contact_id, 
-            project_data.npd_study_id
+            study.study_number
           FROM civicrm_case_contact
           INNER JOIN civicrm_value_nihr_participation_data participation_data ON participation_data.entity_id = civicrm_case_contact.case_id
           INNER JOIN civicrm_value_nihr_project_data project_data ON project_data.entity_id = participation_data.nvpd_project_id
+          INNER JOIN civicrm_nihr_study study ON study.id = project_data.npd_study_id
           WHERE civicrm_case_contact.case_id = %1";
+
     $sqlParams[1] = array($case_id, 'Integer');
     $dao = CRM_Core_DAO::executeQuery($sql, $sqlParams);
     while($dao->fetch()) {
-      $id = self::generateNumber($dao->npd_study_id, $dao->contact_id, $case_id);
+      $id = self::generateNumber($dao->study_number, $dao->contact_id, $case_id);
       // We assume a record already exists for this case.
       CRM_Core_DAO::executeQuery("UPDATE civicrm_value_nihr_participation_data SET nvpd_study_participant_id = %1 WHERE entity_id = %2", array(
         1 => array($id, 'String'),
