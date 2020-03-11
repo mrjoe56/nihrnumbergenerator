042db6dd32df02d27a3bead9f5f9dba0038f3225
ErikHommel <erik.hommel@civicoop.org>
2020-03-11 11:54:01 +0100

    fix issue 6222 - sequence numbers cbr and nbr

--- CRM/Nihrnumbergenerator/StudyNumberGenerator.php
+++ CRM/Nihrnumbergenerator/StudyNumberGenerator.php
@@ -17,13 +17,21 @@ class CRM_Nihrnumbergenerator_StudyNumberGenerator {
     // only if study campaign type
     if (isset($objectRef->campaign_type_id)) {
       if ($objectRef->campaign_type_id == CRM_Nihrbackbone_BackboneConfig::singleton()->getStudyCampaignTypeId()) {
-        $prefix = 'NBR';
         $centre = CRM_Nihrbackbone_NbrStudy::getCentreOfOrigin($studyId);
         if ($centre && $centre == CRM_Nihrnumbergenerator_Config::singleton()->cambridgeCenterName) {
           $prefix = 'CBR';
+          $sequence = Civi::settings()->get('nbr_cbr_sequence');
+          $sequence++;
+          Civi::settings()->set('nbr_cbr_sequence', $sequence);
+        }
+        else {
+          $prefix = 'NBR';
+          $sequence = Civi::settings()->get('nbr_nbr_sequence');
+          $sequence++;
+          Civi::settings()->set('nbr_nbr_sequence', $sequence);
         }
         // add prefix to id and save in study number field
-        $studyNumber = $prefix . $studyId;
+        $studyNumber = $prefix . $sequence;
         $studyNumberCustomField = "custom_" . CRM_Nihrbackbone_BackboneConfig::singleton()->getStudyCustomField('nsd_study_number', 'id');
         $apiParams = [
           'id' => $studyId,
